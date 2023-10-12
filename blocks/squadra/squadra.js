6c24ffa1e018220c03c6276cd44278b3f2276d8e
Marco Pasini <mpasini@adobe.com>
2023-10-12 17:09:10 +0200

    table for standings

---
+++ blocks/squadra/squadra.js
@@ -0,0 +1,16 @@
+export default function decorate(block) {
+    let children = block.children;
+    debugger;
+    for(let i=0; i<children.length; i++){
+        let child = children[i];
+        child.classList.add("table-row");
+        if (i==0) child.classList.add("table-header");
+
+        let children2 = child.children;
+        for(let j=0; j<children2.length; j++){
+            let child2 = children2[j];
+            child2.classList.add("table-cell");
+        }
+    }
+}
+  
